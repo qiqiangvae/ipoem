bf40a77cf3a4efc308833f2bc6cfe87e28563df1
qiqiang <qiqiangvae@foxmail.com>
2019-07-10 10:04:21 +0800

    修复h5无法启动bug

--- src/main/java/org/qiqiang/ipoem/spider/PoemPageProcessor.java
+++ src/main/java/org/qiqiang/ipoem/spider/PoemPageProcessor.java
@@ -1,4 +1,18 @@
 package org.qiqiang.ipoem.spider;
 
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.processor.PageProcessor;
+
 public class PoemPageProcessor implements PageProcessor {
+
+    @Override
+    public void process(Page page) {
+
+    }
+
+    @Override
+    public Site getSite() {
+        return null;
+    }
 }
