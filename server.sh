4d1688e7862ce0422462354959cc41449587f179
qiqiang <qiqiangvae@foxmail.com>
2020-10-13 14:53:12 +0800

    启动脚本修改

--- server.sh
+++ server.sh
@@ -2,7 +2,7 @@
 # 后端部署
 echo '编译后端'
 mvn -DskipTests=true  clean package
-cp target/ipoem-*.jar /Workspack/chinese_poetry/
+cp target/ipoem-*.jar /Workspace/chinese_poetry/
 nohup java -Xms256m -Xmx256m  -jar ipoem-0.0.2-SNAPSHOT.jar > chinese_poetry.log 2>&1 &
 echo '后台启动成功'
 # 前端打包
--- src/ipoem-h5/static/config/config.js
+++ src/ipoem-h5/static/config/config.js
@@ -1,7 +1,7 @@
 const dev = {
   // baseURL: 'http://localhost:8111'
-  baseURL: 'http://192.168.37.8:8111'
-  // baseURL: 'http://116.85.33.111:8111'
+  // baseURL: 'http://192.168.37.8:8111'
+  baseURL: 'http://116.85.33.111:8111'
 }
 
 export default dev
