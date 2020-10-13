1e33eb23a22e72a523e149c6bf48f3e8b8535320
qiqiang <qiqiangvae@foxmail.com>
2020-10-13 18:36:21 +0800

    新增启动脚本

--- server.sh
+++ server.sh
@@ -6,70 +6,79 @@ USAGE="Usage: startup.sh {start|stop|restart}"
 exec_param_count=$#
 # 如果不是 stop 命令，那么就是需要启动启动服务，此时参数不能少于3个，如果小于3，则提示使用命令
 if [ $1 != "stop" -a $exec_param_count -lt 1 ]; then
-  echo $USAGE
-  exit
+    echo $USAGE
+    exit
 fi
 
 cd $(dirname $0)
 WORKSPACE=$(pwd)
+# 获取 jar 文件
+JAR_NAME=$(ls $WORKSPACE | grep .jar$ | awk '{print "'$LIB_DIR'/"$0}')
+FULL_JAR_NAME="$WORKSPACE$JAR_NAME"
 
 # 启动命令
 start() {
-  # 根据配置文件找出 pid
-  echo "WORKSPACE=$WORKSPACE"
-  PIDS=$(ps -ef | grep java | grep -v grep | grep "$WORKSPACE" | awk '{print $2}')
-  if [ -n "$PIDS" ]; then
-    echo "ERROR: The server already started!"
-    echo "PID: $PIDS"
-    exit 1
-  fi
-
-  # 获取 jar 文件
-  JAR_NAME=$(ls $WORKSPACE | grep .jar$ | awk '{print "'$LIB_DIR'/"$0}')
-  FULL_JAR_NAME="$WORKSPACE$JAR_NAME"
-
-  # 启动
-  echo "准备启动$FULL_JAR_NAME"
-  echo "nohup java -Xmx256m -Xms256m -Xmn256m -jar $FULL_JAR_NAME >/dev/null 2>&1 &"
-  nohup java -Xmx256m -Xms256m -Xmn256m -jar "$FULL_JAR_NAME" >server.log 2>&1 &
-
-  # 等待启动
-  COUNT=0
-  WAIT_TIME=0
-  while [ $COUNT -lt 1 ]; do
-    echo "......"
-    sleep 1
-    WAIT_TIME=$(expr $WAIT_TIME + 1)
-    COUNT=$(ps -ef | grep java | grep -v grep | grep "$FULL_JAR_NAME" | awk '{print $2}' | wc -l)
-    if [ $COUNT -gt 0 ]; then
-      break
+    # 根据配置文件找出 pid
+    echo "WORKSPACE=$WORKSPACE"
+    PIDS=$(ps -ef | grep java | grep -v grep | grep "$WORKSPACE" | awk '{print $2}')
+    if [ -n "$PIDS" ]; then
+        echo "启动失败: 该服务已经在运行中!PID: $PIDS"
+        exit 1
     fi
-    if [ $WAIT_TIME -gt 120 ]; then
-      echo "启动超时"
-      exit
+
+    # 启动
+    echo "准备启动$FULL_JAR_NAME"
+    echo "nohup java -Xmx256m -Xms256m -Xmn256m -jar $FULL_JAR_NAME >/dev/null 2>&1 &"
+    nohup java -Xmx256m -Xms256m -Xmn256m -jar "$FULL_JAR_NAME" >server.log 2>&1 &
+
+    # 等待启动完成
+    COUNT=0
+    WAIT_TIME=0
+    echo "...................................."
+    while [ $COUNT -lt 1 ]; do
+        sleep 1
+        WAIT_TIME=$(expr $WAIT_TIME + 1)
+        COUNT=$(ps -ef | grep java | grep -v grep | grep "$FULL_JAR_NAME" | awk '{print $2}' | wc -l)
+        if [ $COUNT -gt 0 ]; then
+            break
+        fi
+        if [ $WAIT_TIME -gt 120 ]; then
+            echo "启动超时"
+            exit 1
+        fi
+    done
+    echo "...................................."
+    PIDS=$(ps -ef | grep java | grep -v grep | grep "$FULL_JAR_NAME" | awk '{print $2}')
+    echo "服务启动成功，PID: $PIDS"
+}
+
+stop() {
+    # 根据配置文件找出 pid
+    echo "WORKSPACE=$WORKSPACE"
+    PIDS=$(ps -ef | grep java | grep -v grep | grep "$FULL_JAR_NAME" | awk '{print $2}')
+    if [ -z "$PIDS" ]; then
+        echo "该服务尚未运行!"
+        exit
     fi
-  done
-  echo "............"
-  echo "The server start OK!"
-  PIDS=$(ps -ef | grep java | grep -v grep | grep "$FULL_JAR_NAME" | awk '{print $2}')
-  echo "PID: $PIDS"
+    kill $PIDS
+    echo "服务停止,PID:$PIDS"
 }
 
 case $1 in
 start)
-  start
-  ;;
+    start
+    ;;
 stop)
-  stop
-  ;;
+    stop
+    ;;
 restart)
-  echo "############ Application of '"$MAIN_CLASS"' restarting....############"
-  stop
-  sleep 1
-  start
-  ;;
+    echo "服务重启中"
+    stop
+    sleep 1
+    start
+    ;;
 *)
-  echo $USAGE
-  ;;
+    echo $USAGE
+    ;;
 esac
 exit 0
