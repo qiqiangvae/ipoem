2dcd94a9fcc087ed188c1293ec5a69d4b0d7cc6e
qiqiang <qiqiangvae@foxmail.com>
2020-10-13 14:37:30 +0800

    启动脚本修改

--- server.sh
+++ server.sh
@@ -1,7 +1,11 @@
 #!/bin/zsh
 # 后端部署
-cd ipoem & mvn -DskipTests=true  clean package
+echo '编译后端'
+mvn -DskipTests=true  clean package
 cp target/ipoem-*.jar /Workspack/chinese_poetry/
 nohup java -Xms256m -Xmx256m  -jar ipoem-0.0.2-SNAPSHOT.jar > chinese_poetry.log 2>&1 &
+echo '后台启动成功'
 # 前端打包
-cd src/ipoem-h5 & npm install & npm run build & cp src/ipoem-h5/build /Workspack/chinese_poetry/build
+echo '打包前端'
+cd src/ipoem-h5 & npm install & npm run build & cp src/ipoem-h5/dist /Workspace/chinese_poetry/dist
+echo '前端打包完成'
